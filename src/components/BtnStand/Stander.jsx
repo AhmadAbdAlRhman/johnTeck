e1b252a9a6d7db9db504863a1e82981248bd54f4
AhmadAbdAlRhman <ahmadhajnajeeb45@gmail.com>
2024-12-15 23:49:41 +0300

    first commit

---
+++ src/components/BtnStand/Stander.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./Stander.css";
+
+const Stander = (props) => {
+  const [showData, setShowData] = useState(false) ;// تظهر وتخفي الداتا 
+
+  const handleButtonClick = (e) => {
+    e.stopPropagation();
+    setShowData(true);
+  };
+
+  const handleHideData = (e) => {
+    e.stopPropagation();
+    setShowData(false);
+  };
+
+  return (
+    <div className="botton_item" onClick={handleHideData}>
+      {/* عرض الزر فقط إذا كانت حالة isButtonVisible true */}
+        <button
+          className="botton2 iner"
+          type="submit"
+          onClick={handleButtonClick}
+        >
+          Standers
+        </button>
+
+      {/* عرض النافذة المنبثقة */}
+      {showData && props.standardData && (
+        <>
+          {/* نافذة منبثقة */}
+          <div className="popup" onClick={(e) => e.stopPropagation()}>
+            <button onClick={handleHideData}>&times;</button>
+            <div style={{ marginTop: "10px", textAlign: "left" }}><h2 className="Stand1">Standard Information</h2>
+              <p className="Stand2">{props.standardData}</p> 
+            </div>
+          </div>
+
+          {/* Overlay */}
+          <div className="overlay" onClick={handleHideData}></div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Stander;
